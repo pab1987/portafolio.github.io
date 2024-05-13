9d4e3b8319448def9b7ff7cc3a57606d25ae4de1
Pablo Lara <pablo06lara87@gmail.com>
2024-05-13 17:44:58 -0500

    ajustes en el js

--- js/index.js
+++ js/index.js
@@ -6,8 +6,6 @@ window.onload = function () {
 
 const contactoEmail = document.getElementById('contacto-email');
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+contactoEmail.setAttribute('href', 'mailto:?subject=Solicitud%20de%20información%20desde%20tu%20portafólio&body=Hola,%20me%20gustaría%20obtener%20información%20sobre%20tu%20perfil');
 
-if (isMobile) {
-    contactoEmail.setAttribute('href', 'mailto:?subject=Contácto%20sobre%tu%20portafólio&body=Hola,%20me%20gustaría%20obtener%20información%20sobre%20tu%perfil');
 
-}
